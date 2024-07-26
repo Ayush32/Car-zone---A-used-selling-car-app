070fc4c59ff10078c9d7755a52430aade4c77464
Ayush Gupta <721ayush@gmail.com>
2024-07-27 00:02:06 +0530

    add media file and configuration

--- carzone/urls.py
+++ carzone/urls.py
@@ -1,23 +1,21 @@
-"""
-URL configuration for carzone project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # This line routes requests starting with 'admin/' to the Django admin site.
+    # When a user accesses 'http://yourdomain.com/admin/', they will be directed to the Django admin interface.
     path('admin/', admin.site.urls),
-    path('',include('pages.urls')),
-]
+    
+    # This line includes the URLs defined in the 'pages' app's 'urls.py' file when the path is empty.
+    # This means that if a user accesses the root URL 'http://yourdomain.com/', Django will further route the request
+    # based on the URLs defined in the 'pages' app.
+    path('', include('pages.urls')),
+] 
+
+# This part is used to serve media files during development.
+# It appends a URL pattern for serving media files uploaded by users.
+# 'settings.MEDIA_URL' specifies the URL path to serve the media files from,
+# and 'settings.MEDIA_ROOT' specifies the directory on the filesystem where the uploaded media files are stored.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length =255)
+    last_name = models.CharField(max_length =255) 
+    designation = models.CharField(max_length =255)
+    # 'photos/%Y/%m/%d/' - this will upload image with system date in yyyy/mm/dd
+    avatar = models.ImageField(upload_to = 'photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_plus_link = models.URLField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    # store the team with first name in db instead of db object 
+    def __str__(self) -> str:
+        return self.first_name
