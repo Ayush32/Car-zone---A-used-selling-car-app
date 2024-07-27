a32696fd1b7ab784b8cf5ac08c3730c62679c763
Ayush Gupta <721ayush@gmail.com>
2024-07-27 11:57:23 +0530

    customization of admin interface display teams data in form of table, on django admin dashboard

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import format_html
+# customize the admin page
+
+class TeamAdmin(admin.ModelAdmin):
+    
+    def thumbnail(self,object):
+        return format_html('<img src = "{}" width = "40" style = "border-radius : 50%" />'.format(object.avatar.url))
+    
+    thumbnail.short_description = 'avatar'
+    
+    # display the data in table
+    list_display = ('id','thumbnail','first_name','last_name','designation','created_date')
+    
+    # to open the data by click on first_name
+    list_display_links = ('id','first_name',)
+    
+    # to search with specific paramter like name,designation in django admin db
+    search_fields = ('first_name','id','designation')
+    
+    # filter the data using particular designation
+    list_filter = ('designation',)
+
+
 # Register your models here.
-admin.site.register(Team)
+admin.site.register(Team,TeamAdmin)
+
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'pages/home.html')
+    team = Team.objects.all()
+    data = {
+        'teams' : team,
+        
+    }
+    return render(request,'pages/home.html',data)
 
 
 def about(request):
-    return render(request,'pages/about.html')
+    team = Team.objects.all()
+    data = {
+        'teams' : team,
+        
+    }
+    return render(request,'pages/about.html',data)
 
 
 def services(request):
@@ -14,3 +24,5 @@ def services(request):
 
 def contact(request):
     return render(request,'pages/contact.html')
+
+# def getAllTeam()
